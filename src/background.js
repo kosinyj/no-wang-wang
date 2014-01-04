8f1b32c8337d27c77195d38c6f2458910439f773
Hsin-Yu Ko <kosinyj@gmail.com>
2014-01-04 11:43:16 -0500

    timer add

--- src/background.js
+++ src/background.js
@@ -1,19 +1,23 @@
 var listener = function(details) {
-  if (details.url.indexOf("blog.chinatimes.com") !== -1) {
-    return {};
-  } else {
-    return {
-      cancel: true
-    };
+  if ( (new Date()).getHours()>8 && (new Date()).getHours()<19){
+    if (details.url.indexOf("blog.chinatimes.com") !== -1) {
+      return {};
+    } else {
+      return {
+        cancel: true
+      };
+    }
   }
 };
 
 var urls = [
   "*://*.facebook.com/*",
   "*://*.yahoo.com/*",
-  "*://*.chinatimes.com/*",
-  "*://*.ctitv.com.tw/*",
-  "*://*.ctv.com.tw/*"
+//   "*://*.chinatimes.com/*",
+//   "*://*.ctitv.com.tw/*",
+//   "*://*.ctv.com.tw/*"
 ];
 
-chrome.webRequest.onBeforeRequest.addListener(listener, { urls: urls }, [ "blocking" ]);
+var dd = new Date();
+
+chrome.webRequest.onBeforeRequest.addListener(listener, { urls: urls },  [ "blocking" ]);
